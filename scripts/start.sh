ecdf227860a09d75d34a8f6ff85503269fee51d6
juniuszhou <junius.zhou@gmail.com>
2022-03-28 23:03:16 +0800

    update start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,15 @@
-# start the wasmd local
-wasmd start
+rm -rf "$HOME"/.wasmd
+rm -rf /tmp/wasmd.log
 
-# 
+# init the wasmd config in ~/.wasmd/
+wasmd init --chain-id test admin
+# add validator for block production
+wasmd keys add validator --keyring-backend test 
+# add genesis account and its tokens
+wasmd add-genesis-account validator "1000000000stake,1000000000cosmos" --keyring-backend test 
+# tx for validator to staking
+wasmd gentx validator "250000000stake" --chain-id=test --amount="250000000stake" --keyring-backend test 
+# collect tx for start the chain
+wasmd collect-gentxs
+# start the chain
+wasmd start --rpc.laddr tcp://localhost:26657 --trace > /tmp/wasmd.log  2>&1 &
